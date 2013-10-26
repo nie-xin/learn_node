f201fbfcd9162cf2f91f754ae70cdbe3e5911fe4
nie xin <niexin.study@gmail.com>
2013-10-26 22:52:28 +0200

    start ch5

---
+++ microblog/app.js
@@ -0,0 +1,41 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+// routers
+app.get('/', routes.index);
+app.get('/hello', routes.hello);
+app.get('/user/:username', function(req, res) {
+	res.send('user: ' + req.params.username);
+});
+app.get('/users', user.list);
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
